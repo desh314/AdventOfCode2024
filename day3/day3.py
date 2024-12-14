bf899ae468a7ac9dd3f4835e5118d8b0d8bbd107
desh <anirdesh.shankar28@gmail.com>
2024-12-14 14:28:13 +0000

    Day 3

--- day3/day3.py
+++ day3/day3.py
@@ -1,9 +1,51 @@
-file = open("input.txt", "r").readlines()[0]
+file = open("input.txt", "r").read()
 
-print(file)
 pos = []
 
-for index, value in enumerate(file):
-    if value+file[index+1]+file[index+2] == "mul":
-        pos.append(file[i:i+])
+""" out = 0
 
+def getNum (index):
+    num = ""
+    while (ord('0') <= ord(file[index]) <= ord('9')):
+        num += file[index]
+        index += 1
+    return num, index
+
+for index, value in enumerate(file):
+    if file[index:index+4] == "mul(":
+        index += 4
+        num, newIndex = getNum(index)
+        if file[newIndex] != "," or num == "":
+            pass
+        else:
+            n1 = int(num)
+            num, newIndex = getNum(newIndex+1)
+            n2 = int(num)
+            if file[newIndex] != ")":
+                pass
+            else:
+                out += n1*n2 """
+
+
+
+out = 0
+
+def checkNum(string):
+    for i in string:
+        if not (ord('0') <= ord(i) <= ord('9')):
+            return False
+    
+    return True
+
+
+pS = [i.split(")")[0].split(",") for i in file.split("mul(")]
+
+for i in pS:
+    if len(i) != 2 or not(checkNum(i[0])) or not(checkNum(i[1])):
+        pass
+    else:
+        out += int(i[0]) * int(i[1])
+
+        
+
+print(out)       
